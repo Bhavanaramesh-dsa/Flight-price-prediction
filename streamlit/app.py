9cb8685482451aedff5cb17c996cfacbac22d361
Prabhaker <prabhakarsuresh1706@gmail.com>
2025-10-08 21:26:03 +0200

    modified

--- streamlit/app.py
+++ streamlit/app.py
@@ -4,15 +4,14 @@ import pickle
 from catboost import CatBoostRegressor
 import os
 
-# ===============================
 # 1. Load model and columns
-# ===============================
+
 model_path = "model/catboost_flight_price_model.cbm"
 columns_path = "model/model_columns.pkl"
 
 # Check if files exist
 if not os.path.exists(model_path) or not os.path.exists(columns_path):
-    st.error("❌ Model or columns file not found! Please train the model first.")
+    st.error(" Model or columns file not found! Please train the model first.")
     st.stop()
 
 # Load CatBoost model
